0eab33afb58a1810e55dc91507165a47b8430ade
최성식 <sungsik81@gmail.com>
2017-04-13 19:55:59 +0900

    First commit

---
+++ TryTravisCITests/TryTravisCITests.m
@@ -0,0 +1,33 @@
+//
+//  TryTravisCITests.m
+//  TryTravisCITests
+//
+//  Created by Krister Kari on 18.3.2014.
+//  Copyright (c) 2014 Krister Kari. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(TryTarvisCITests)
+
+describe(@"In our first Kiwi test", ^{
+    context(@"a sample string", ^{
+
+        NSString *greeting = @"Hello, World!";
+    
+        it(@"should exist.", ^{
+            [greeting shouldNotBeNil];
+        });
+
+        it(@"should be 'Hello, World!'.", ^{
+            [[greeting should] equal:@"Hello, World!"];
+        });
+
+        it(@"should not be 'Hello'.", ^{
+            [[greeting shouldNot] equal:@"Hello"];
+        });
+        
+    });
+});
+
+SPEC_END
